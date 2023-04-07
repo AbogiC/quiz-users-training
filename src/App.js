734a6b4b08e6bb34ed15dfb7b2b9d33f74d3292f
psmabedn7227 <abednego.bogichristian@ai.astra.co.id>
2023-04-07 23:05:09 +0700

    connect with database

--- src/App.js
+++ src/App.js
@@ -1,82 +1,71 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { db } from "./firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
 export default function App() {
-	const questions = [
-		{
-			questionText: 'What is the capital of France?',
-			answerOptions: [
-				{ answerText: 'New York', isCorrect: false },
-				{ answerText: 'London', isCorrect: false },
-				{ answerText: 'Paris', isCorrect: true },
-				{ answerText: 'Dublin', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Who is CEO of Tesla?',
-			answerOptions: [
-				{ answerText: 'Jeff Bezos', isCorrect: false },
-				{ answerText: 'Elon Musk', isCorrect: true },
-				{ answerText: 'Bill Gates', isCorrect: false },
-				{ answerText: 'Tony Stark', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'The iPhone was created by which company?',
-			answerOptions: [
-				{ answerText: 'Apple', isCorrect: true },
-				{ answerText: 'Intel', isCorrect: false },
-				{ answerText: 'Amazon', isCorrect: false },
-				{ answerText: 'Microsoft', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'How many Harry Potter books are there?',
-			answerOptions: [
-				{ answerText: '1', isCorrect: false },
-				{ answerText: '4', isCorrect: false },
-				{ answerText: '6', isCorrect: false },
-				{ answerText: '7', isCorrect: true },
-			],
-		},
-	];
+  const [questionsData, setQuestions] = useState([]);
+  const questionCollectionRef = collection(db, "questions");
+
+  useEffect(() => {
+    const getQuestions = async () => {
+      const data = await getDocs(questionCollectionRef);
+      setQuestions(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getQuestions();
+  }, []);
 
 	const [currentQuestion, setCurrentQuestion] = useState(0);
 	const [showScore, setShowScore] = useState(false);
 	const [score, setScore] = useState(0);
 
-	const handleAnswerOptionClick = (isCorrect) => {
-		if (isCorrect) {
+	const handleAnswerOptionClick = (answer, correctAnswer) => {
+		if (answer === correctAnswer) {
 			setScore(score + 1);
 		}
 
 		const nextQuestion = currentQuestion + 1;
-		if (nextQuestion < questions.length) {
+		if (nextQuestion < questionsData.length) {
 			setCurrentQuestion(nextQuestion);
 		} else {
 			setShowScore(true);
 		}
 	};
+
 	return (
-		<div className='app'>
-			{showScore ? (
-				<div className='score-section'>
-					You scored {score} out of {questions.length}
-				</div>
-			) : (
-				<>
-					<div className='question-section'>
-						<div className='question-count'>
-							<span>Question {currentQuestion + 1}</span>/{questions.length}
-						</div>
-						<div className='question-text'>{questions[currentQuestion].questionText}</div>
-					</div>
-					<div className='answer-section'>
-						{questions[currentQuestion].answerOptions.map((answerOption) => (
-							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
-						))}
-					</div>
-				</>
-			)}
-		</div>
+    <div className='app'>
+      {showScore ? (
+        <div className='score-section'>
+          You scored {score} out of {questionsData.length}
+        </div>
+      ) : (
+        <>
+        {questionsData.map((quest, i) => {
+          if(i === currentQuestion) {
+            return (
+              <>
+                <div className='question-section'>
+                  <div className='question-count'>
+                    {" "}
+                    <span>Question {currentQuestion + 1}</span>/{questionsData.length}
+                  </div>
+                  <div className='question-text'>
+                    {quest.question}
+                  </div>
+                </div>
+                <div className='answer-section'>
+                  <button onClick={() => handleAnswerOptionClick(quest.answer1, quest.correct)}>{quest.answer1}</button>
+                  <button onClick={() => handleAnswerOptionClick(quest.answer2, quest.correct)}>{quest.answer2}</button>
+                  <button onClick={() => handleAnswerOptionClick(quest.answer3, quest.correct)}>{quest.answer3}</button>
+                  <button onClick={() => handleAnswerOptionClick(quest.answer4, quest.correct)}>{quest.answer4}</button>
+                </div>
+              </>
+            )
+          }
+        })}
+        </>
+      )}
+    </div>
 	);
 }
